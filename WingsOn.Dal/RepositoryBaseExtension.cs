2eff46a22ad7b73b48806cce79f41c4dd20f4ab8
Mohamed Salah <eng.mohamed.tawab@gmail.com>
2019-06-19 14:34:52 +0200

    adding comments

--- WingsOn.Dal/RepositoryBaseExtension.cs
+++ WingsOn.Dal/RepositoryBaseExtension.cs
@@ -13,19 +13,38 @@ namespace WingsOn.Dal
         //this is use for generating the booking id
         private static readonly Object lockObj = new Object();
 
-
+        /// <summary>
+        /// Wheres the specified predict.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="repository">The repository.</param>
+        /// <param name="predict">The predict.</param>
+        /// <returns></returns>
         public static IEnumerable<T> Where<T>(this RepositoryBase<T> repository,Func<T,bool> predict)
         where T : DomainObject
         {
             return repository.GetAll().Where(predict);
         }
+        /// <summary>
+        /// Singles the or default.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="repository">The repository.</param>
+        /// <param name="predict">The predict.</param>
+        /// <returns></returns>
         public static T SingleOrDefault<T>(this RepositoryBase<T> repository, Func<T, bool> predict)
         where T : DomainObject
         {
             return repository.GetAll().SingleOrDefault(predict);
         }
 
-
+        /// <summary>
+        /// Creates the specified new object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="repository">The repository.</param>
+        /// <param name="newObj">The new object.</param>
+        /// <returns></returns>
         public static T Create<T>(this RepositoryBase<T> repository,T newObj) where T : DomainObject
         {
             // critical section
